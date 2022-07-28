02b2f369a556bec7fd08b23ec3913fac3283e66b
luigi311 <git@luigi311.com>
2022-07-28 17:45:26 -0600

    Fix numpy type

--- stacking/stacking.py
+++ stacking/stacking.py
@@ -20,7 +20,7 @@ def stackImagesECCWorker(numpy_array):
     termination_eps = 1e-10
 
     criteria = (
-        cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT,
+        cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
         number_of_iterations,
         termination_eps,
     )
@@ -52,7 +52,7 @@ def stackImagesECCWorker(numpy_array):
             )
 
             # Adjust the warp_matrix to the scale of the original images
-            warp_matrix = warp_matrix * np.array([[1, 1, 1/shrink_factor], [1, 1, 1/shrink_factor], [shrink_factor, shrink_factor, 1]])
+            warp_matrix = (warp_matrix * np.array([[1, 1, 1/shrink_factor], [1, 1, 1/shrink_factor], [shrink_factor, shrink_factor, 1]])).astype(np.float32)
 
             # Align image to first image
             image_align = cv2.warpPerspective(
